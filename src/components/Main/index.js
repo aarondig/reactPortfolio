6192e37c207df10368d3cb85513b67a5eac911ee
Aaron Diggdon <aarondiggdon@gmail.com>
2020-11-18 22:31:06 -0800

    Fixing deployment errors

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,16 +2,15 @@ import "./style.css";
 
 function Main(props) {
   return <div id="main">
-          <div class="ui grid">
-          <div class="one column row">
-              <div class="center aligned column">
-              <div class="ui middle aligned container" id="mainContainer">
-                <h1 class="ui inverted centered header" id="mainHead">AARON DIGGDON</h1>
+          <div class="ui two column centered grid">
+          <div class="center aligned row">
+              <div class="twelve wide center aligned column">
+              <div class="ui container" id="mainContainer">
+                <h1 class="ui inverted header" id="mainHead">AARON DIGGDON</h1>
               </div>
             </div>
-            </div>
-            <div class="column"></div>
-            </div>
+          </div>
+          </div>
           <div id="line"></div> 
           <h1 class="ui header" id="background">AARON DIGGDON AARON DIGGDON AARON DIGGDON</h1>
 
