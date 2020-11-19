2b18e8a6592f621e4f93d9f2de1be8d98ab402b0
Aaron Diggdon <aarondiggdon@gmail.com>
2020-11-18 17:43:00 -0800

    Added fact checker project to Projects

--- portfolio/src/components/Projects/index.js
+++ portfolio/src/components/Projects/index.js
@@ -7,10 +7,46 @@ function Featured(props) {
     <h2 class="ui header">PROJECTS</h2>
       <div class="ui middle aligned stackable grid container">
       <div class='row'>
-        <div class='ten wide column'>
+        <div class='ten wide centered column'>
           <h3>Born and raised in Northern California</h3>
         </div>
       </div>
+
+      <div class="row">
+        <div class="fourteen wide centered column">
+          <a href="https://aarondig.github.io/factCheck/">
+            <img class="img-fluid" src="https://github.com/aarondig/factCheck/blob/main/assets/banner.png?raw=true"></img>
+          </a>
+        </div>
+        <div class="fourteen wide centered column">
+          <div class='column'>
+            <h2 class="title">Google Fact Checker</h2>
+          </div>
+          <div class="column">
+            <h3>Using Google's Fact Check API, this application ruturns fact checked articles based on the user's input search query. Using CSS and jquery animations, this appliaction displays the capabilities of my coding and design skillset.</h3>
+            <hr/>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="fourteen wide centered column">
+          <a href="https://aarondig.github.io/factCheck/">
+            <img class="img-fluid" src="https://github.com/aarondig/factCheck/blob/main/assets/banner.png?raw=true"></img>
+          </a>
+        </div>
+        <div class="fourteen wide centered column">
+          <div class='column'>
+            <h2 class="title">Google Fact Checker</h2>
+          </div>
+          <div class="column">
+            <h3>Using Google's Fact Check API, this application ruturns fact checked articles based on the user's input search query. Using CSS and jquery animations, this appliaction displays the capabilities of my coding and design skillset.</h3>
+            <hr/>
+          </div>
+        </div>
+      </div>
+      
+          
     </div>
   </div>
 </div> ;
