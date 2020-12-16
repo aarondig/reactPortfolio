904201ecfc17cec65c537980f1213b1fe4916bed
Aaron Diggdon <aarondiggdon@gmail.com>
2020-12-15 18:25:11 -0800

    Added onScroll fade to mainContainer

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,19 +1,36 @@
+import React, { useState, useEffect } from "react";
 import "./style.css";
 
 function Main(props) {
+
+  const [scrolling, setScrolling] = useState(false);
+  const [scrollTop, setScrollTop] = useState(0);
+
+  useEffect(() => {
+    const onScroll = e => {
+      setScrollTop(e.target.documentElement.scrollTop);
+      if (scrollTop < 300) {
+      setScrolling( e.target.documentElement.scrollTop > scrollTop);
+    }
+    };
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollTop]);
+
+  const style = scrolling ? { opacity: 0 } : { opacity: 1 };
+
   return <div id="main">
-          <div class="ui two column centered grid">
-          <div class="center aligned row">
-              <div class="twelve wide center aligned column">
-              <div class="ui container" id="mainContainer">
-                <h1 class="ui inverted header" id="mainHead">AARON DIGGDON</h1>
-                <h2 class="ui inverted header" id="subHead">User experience driven web development.</h2>
-              </div>
+          <div className="ui two column centered grid">
+          <div className="center aligned row">
+              <div className="ui container" id="mainContainer" style={style}>
+                <h1 className="ui inverted header" id="mainHead">AARON DIGGDON</h1>
+                <h2 className="ui inverted header" id="subHead">User experience driven web development.</h2>
             </div>
           </div>
           </div>
           <div id="line"></div>  
-          <h1 class="ui header" id="background">AARON DIGGDON AARON DIGGDON AARON DIGGDON</h1>
+          <h1 className="ui header" id="background">AARON DIGGDON AARON DIGGDON AARON DIGGDON</h1>
 
         </div>;
 }
